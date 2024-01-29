35375bf16c15c737dfe7e381f1b2e87143e1d09e
Juuso Elo-Rauta <juuso.elo-rauta@wunderdog.fi>
2024-01-29 14:56:34 +0200

    remove next app and ui package and add svelte client app

--- apps/web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Lander(): JSX.Element {
-  const loggedIn = false;
-  const btnText = loggedIn ? "Logout" : "Login";
-  return <div>
-    <h1>Moi :)</h1>
-    <button></button>
-  </div>
-}
